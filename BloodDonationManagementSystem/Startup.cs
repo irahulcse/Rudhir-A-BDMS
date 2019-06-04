ea37364ff4c00e2d49fdad258ed7eb9f441c6eaa
Dhruv Kinger <cse1785@outlook.com>
2019-06-04 18:18:26 +0530

    Add project files.

---
+++ BloodDonationManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BloodDonationManagementSystem.Startup))]
+namespace BloodDonationManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
